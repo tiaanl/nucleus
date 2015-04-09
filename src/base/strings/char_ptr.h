8e7249fc3b2446403deab5f5f1a01be8876a255d
Tiaan Louw <tiaanl@gmail.com>
2015-04-09 10:10:49 +0200

    Added CharPtrTraits for wide characters.

--- src/base/strings/char_ptr.h
+++ src/base/strings/char_ptr.h
@@ -15,10 +15,11 @@
 #ifndef BASE_STRINGS_CHAR_PTR_H_
 #define BASE_STRINGS_CHAR_PTR_H_
 
-#include "base/strings/char_ptr_traits_a.h"
-#include "base/strings/char_ptr_traits_8.h"
 #include "base/strings/char_ptr_traits_16.h"
 #include "base/strings/char_ptr_traits_32.h"
+#include "base/strings/char_ptr_traits_8.h"
+#include "base/strings/char_ptr_traits_a.h"
+#include "base/strings/char_ptr_traits_w.h"
 
 namespace base {
 
@@ -27,9 +28,43 @@ class CharPtrBase {
 public:
   using CharType = typename Traits::CharType;
 
+  // Return the number of bytes required to represent the given character in
+  // this encoding.
+  static uint8_t bytesRequiredFor(CharType ch) {
+    return Traits::bytesRequiredFor(ch);
+  }
+
+  // Construct a CharPtrBase which is wrapping a null pointer.
   CharPtrBase() {}
 
+  // Construct a CharPtrBase which points to the given pointer.
   CharPtrBase(const CharType* ptr) : m_ptr(const_cast<CharType*>(ptr)) {}
+
+  // Comparison operators.
+
+  bool operator==(const CharPtrBase& other) const {
+    return m_ptr == other.m_ptr;
+  }
+
+  bool operator!=(const CharPtrBase& other) const {
+    return m_ptr != other.m_ptr;
+  }
+
+  bool operator<(const CharPtrBase& other) const {
+    return m_ptr < other.m_ptr;
+  }
+
+  bool operator<=(const CharPtrBase& other) const {
+    return m_ptr <= other.m_ptr;
+  }
+
+  bool operator>(const CharPtrBase& other) const {
+    return m_ptr > other.m_ptr;
+  }
+
+  bool operator>=(const CharPtrBase& other) const {
+    return m_ptr >= other.m_ptr;
+  }
 
   // Return the unicode character that we are currently pointing to.
   char32_t getUnicodeChar() const { return Traits::getUnicodeChar(m_ptr); }
@@ -37,17 +72,13 @@ public:
   // Return true if we are pointing to a null terminator.
   bool isNull() const { return getUnicodeChar() == 0; }
 
-  // Return the number of bytes required to represent the given character in
-  // this encoding.
-  static uint8_t bytesRequiredFor(CharType ch) {
-    return Traits::bytesRequiredFor(ch);
-  }
-
 private:
   CharType* m_ptr{nullptr};
 };
 
 using CharPtrA = CharPtrBase<CharPtrTraitsA>;
+using CharPtrW = CharPtrBase<CharPtrTraitsW>;
+
 using CharPtr8 = CharPtrBase<CharPtrTraits8>;
 using CharPtr16 = CharPtrBase<CharPtrTraits16>;
 using CharPtr32 = CharPtrBase<CharPtrTraits32>;
