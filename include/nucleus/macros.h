371ace2272c1afefe5518f93839e5b6b7e7cc7ec
Tiaan Louw <tiaanl@gmail.com>
2015-04-23 17:13:06 +0200

    Added useful macros.

--- include/nucleus/macros.h
+++ include/nucleus/macros.h
@@ -15,6 +15,8 @@
 #ifndef NUCLEUS_MACROS_H_
 #define NUCLEUS_MACROS_H_
 
+#include "nucleus/config.h"
+
 #define DISALLOW_COPY_AND_ASSIGN(ClassName) \
   ClassName(const ClassName&) = delete; \
   ClassName& operator=(const ClassName&) = delete
@@ -22,5 +24,21 @@
 #define DISALLOW_IMPLICIT_CONSTRUCTORS(ClassName) \
   DISALLOW_COPY_AND_ASSIGN(ClassName); \
   ClassName() = delete
+
+// ARRAY_SIZE
+
+// Helper to figure out the item count of a static array of elements, c++ style!
+template <typename T, size_t N>
+char(&ArraySizeHelper(T(&array)[N]))[N];
+#if COMPILER(MSVC)
+template <typename T, size_t N>
+char(&ArraySizeHelper(const T(&array)[N]))[N];
+#endif  // COMPILER(MSVC)
+
+#define ARRAY_SIZE(array) (sizeof(ArraySizeHelper(array)))
+
+// IS_BIT_SET
+
+#define IS_BIT_SET(Value, Bit) ((Value & Bit) == Bit)
 
 #endif  // NUCLEUS_MACROS_H_
