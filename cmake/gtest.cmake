193e4d0448698818a6279317e46a9046d1f61b65
Tiaan Louw <tiaanl@gmail.com>
2015-05-08 20:34:43 +0200

    Changed gtest include directory to be reusable.

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,5 +1,5 @@
 
 add_subdirectory("third_party/gtest-1.7.0" "gtest")
-include_directories("${gtest_SOURCE_DIR}/include")
+target_include_directories("gtest" PUBLIC "${gtest_SOURCE_DIR}/include")
 set_property(TARGET "gtest" PROPERTY FOLDER "gtest")
 set_property(TARGET "gtest_main" PROPERTY FOLDER "gtest")
