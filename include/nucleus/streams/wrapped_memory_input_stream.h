40594359e1b2a8d13e79426b456aefa8479f06e2
Tiaan Louw <tiaanl@gmail.com>
2015-06-13 20:58:24 +0200

    Added WrappedMemoryInputStream.

---
+++ include/nucleus/streams/wrapped_memory_input_stream.h
@@ -0,0 +1,63 @@
+// Copyright (c) 2015, Tiaan Louw
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+// OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+#ifndef NUCLEUS_STREAMS_WRAPPED_MEMORY_INPUT_STREAM_H_
+#define NUCLEUS_STREAMS_WRAPPED_MEMORY_INPUT_STREAM_H_
+
+#include "nucleus/streams/input_stream.h"
+
+#include <vector>
+
+namespace nu {
+
+// MemoryInputStream, that does not make a copy of the data passed to it.
+class WrappedMemoryInputStream : public InputStream {
+public:
+  // Construct the stream with the specified data and size.
+  WrappedMemoryInputStream(const void* data, size_t size);
+
+  // Construct the stream with the specified vector data.
+  explicit WrappedMemoryInputStream(const std::vector<char>& data);
+
+  virtual ~WrappedMemoryInputStream() = default;
+
+  // Return a pointer to the source data block from which this stream is
+  // reading.
+  const void* getData() const { return m_data; }
+
+  // Return the size of the source data (in bytes).
+  size_t getSize() const { return m_size; }
+
+  // Override: InputStream
+  SizeType getPosition() override;
+  bool setPosition(SizeType newPosition) override;
+  SizeType getLength() override;
+  bool isExhausted() override;
+  SizeType read(void* destBuffer, SizeType bytesToRead) override;
+
+private:
+  // A pointer to the start of the data that we are streaming.
+  const void* m_data;
+
+  // The size of the data we are streaming.
+  size_t m_size;
+
+  // The current position of the stream in the source data.
+  size_t m_currentPosition{0};
+
+  DISALLOW_IMPLICIT_CONSTRUCTORS(WrappedMemoryInputStream);
+};
+}  // namespace nu
+
+#endif  // NUCLEUS_STREAMS_WRAPPED_MEMORY_INPUT_STREAM_H_
---
+++ src/streams/wrapped_memory_input_stream.cpp
@@ -0,0 +1,65 @@
+// Copyright (c) 2015, Tiaan Louw
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+// OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+#include "nucleus/streams/wrapped_memory_input_stream.h"
+
+#include <algorithm>
+
+#include "nucleus/logging.h"
+#include "nucleus/types.h"
+
+namespace nu {
+
+WrappedMemoryInputStream::WrappedMemoryInputStream(const void* data,
+                                                   size_t size)
+  : m_data(data), m_size(size) {
+}
+
+WrappedMemoryInputStream::WrappedMemoryInputStream(
+    const std::vector<char>& data)
+  : m_data(data.data()), m_size(data.size()) {
+}
+
+WrappedMemoryInputStream::SizeType WrappedMemoryInputStream::getLength() {
+  return m_size;
+}
+
+WrappedMemoryInputStream::SizeType WrappedMemoryInputStream::read(
+    void* buffer, SizeType bytesToRead) {
+  DCHECK(bytesToRead >= 0);
+
+  SizeType num = std::min(bytesToRead, getBytesRemaining());
+
+  std::memcpy(buffer, static_cast<const u8*>(m_data) + m_currentPosition,
+              static_cast<size_t>(num));
+  m_currentPosition += num;
+
+  return num;
+}
+
+bool WrappedMemoryInputStream::isExhausted() {
+  return m_currentPosition >= m_size;
+}
+
+bool WrappedMemoryInputStream::setPosition(SizeType newPosition) {
+  m_currentPosition = std::max(newPosition, static_cast<SizeType>(0));
+  m_currentPosition = std::min(newPosition, m_size);
+  return true;
+}
+
+WrappedMemoryInputStream::SizeType WrappedMemoryInputStream::getPosition() {
+  return m_currentPosition;
+}
+
+}  // namespace nu
