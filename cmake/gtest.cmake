37ebc8f1046e4a44276126eddbdb173933c34360
Tiaan Louw <tiaanl@gmail.com>
2017-10-27 00:08:16 +0200

    Move gtest to third_party folder

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,7 +1,7 @@
 
 add_subdirectory("third_party/googltest-1.8.0" "gtest")
 target_include_directories("gtest" PUBLIC "${gtest_SOURCE_DIR}/include")
-set_property(TARGET "gtest" PROPERTY FOLDER "gtest")
-set_property(TARGET "gtest_main" PROPERTY FOLDER "gtest")
-set_property(TARGET "gmock" PROPERTY FOLDER "gtest")
-set_property(TARGET "gmock_main" PROPERTY FOLDER "gtest")
+set_property(TARGET "gtest" PROPERTY FOLDER "third_party")
+set_property(TARGET "gtest_main" PROPERTY FOLDER "third_party")
+set_property(TARGET "gmock" PROPERTY FOLDER "third_party")
+set_property(TARGET "gmock_main" PROPERTY FOLDER "third_party")
