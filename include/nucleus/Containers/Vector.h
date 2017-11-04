5909ca7d5b6f57ec39e28e99e21b5bcd57c983c3
Tiaan Louw <tiaanl@gmail.com>
2017-11-05 00:29:49 +0100

    Start building vector class

--- include/nucleus/Containers/Vector.h
+++ include/nucleus/Containers/Vector.h
@@ -2,8 +2,10 @@
 #ifndef NUCLEUS_CONTAINERS_VECTOR_H_
 #define NUCLEUS_CONTAINERS_VECTOR_H_
 
-#include "nucleus/Memory/Allocator.h"
-#include "nucleus/algorithms/copy.h"
+#include <new>
+
+#include "nucleus/Allocators/Allocator.h"
+#include "nucleus/Allocators/DefaultAllocator.h"
 #include "nucleus/types.h"
 
 namespace nu {
@@ -11,10 +13,73 @@ namespace nu {
 template <typename T>
 class Vector {
 public:
-  Vector() {}
-  Vector(Allocator* allocator) {}
+  Vector(Allocator* allocator = getDefaultAllocator())
+    : m_allocator(allocator), m_data(nullptr), m_allocated(0), m_size(0) {}
+
+  Vector(const Vector& other, Allocator* allocator = getDefaultAllocator())
+    : m_size(other.m_size), m_allocator(other.m_allocator) {
+    ensureAllocated(other.m_allocated, false);
+    std::memcpy(m_data, other.m_data, other.m_allocated);
+  }
+
+  Vector(Vector&& other)
+    : m_allocator(other.m_allocator), m_data(other.m_data), m_allocated(other.m_allocated), m_size(other.m_size) {
+    other.m_allocator = nullptr;
+    other.m_data = nullptr;
+    other.m_allocated = 0;
+    other.m_size = 0;
+  }
+
+  Vector(Vector&& other, Allocator* allocator) : Vector(allocator) {
+    if (allocator == m_allocator) {
+      operator=(std::move(other));
+    } else {
+      operator=(other);
+    }
+  }
+
+  ~Vector() {
+    if (m_data) {
+      m_allocator->free(m_data, m_allocated);
+    }
+  }
+
+  USize getSize() const { return m_size; }
+
+  void pushBack(const T& item) {
+    ensureAllocated((m_size + 1) * sizeof(T), true);
+
+    auto pp = m_data[m_size];
+
+    ::new (reinterpret_cast<void*>(&m_data[m_size])) T(item);
+
+    ++m_size;
+  }
 
 private:
+  void ensureAllocated(USize bytes, bool keepOld) {
+    // Allocate a new buffer.
+    T* newData = static_cast<T*>(m_allocator->allocate(bytes));
+
+    // If we should keep the old data, then copy it over.
+    if (m_data) {
+      if (keepOld) {
+        std::memcpy(newData, m_data, std::min(bytes, m_allocated));
+      }
+
+      // Free the old block.
+      m_allocator->free(m_data, m_allocated);
+    }
+
+    // Update the internal state.
+    m_data = newData;
+    m_allocated = bytes;
+  }
+
+  Allocator* m_allocator;
+  T* m_data;
+  USize m_allocated;
+  USize m_size;
 };
 
 }  // namespace nu
--- tests/containers/vector_tests.cpp
+++ tests/containers/vector_tests.cpp
@@ -1,4 +1,23 @@
 
 #include <gtest/gtest.h>
 
-namespace nu {}  // namespace nu
+#include "nucleus/Allocators/DebugAllocator.h"
+#include "nucleus/Allocators/DefaultAllocator.h"
+#include "nucleus/Containers/Vector.h"
+
+TEST(VectorTests, DefaultConstruct) {
+  nu::Vector<int> v;
+}
+
+struct SomeData {
+  int a;
+  int b;
+
+  SomeData(int a, int b) : a(a), b(b) {}
+};
+
+TEST(VectorTests, PushBack) {
+  nu::Vector<SomeData> v;
+  v.pushBack(SomeData{10, 20});
+  EXPECT_EQ(1, v.getSize());
+}
