7d3ea333f37de47dab384dbab0f7f3afad36e7d0
Tiaan Louw <tiaanl@gmail.com>
2017-12-22 22:43:30 +0100

    Add GetCurrentWorkingDirectory for Windows platform

--- src/Utils/Files.cpp
+++ src/Utils/Files.cpp
@@ -4,16 +4,22 @@
 
 #if OS(POSIX)
 #include <unistd.h>
+#elif OS(WIN)
+#include "nucleus/Win/WindowsMixin.h"
 #endif
 
 namespace nu {
 
-#if OS(POSIX)
 FilePath getCurrentWorkingDirectory(Allocator* allocator) {
+#if OS(POSIX)
   char buf[PATH_MAX] = {0};
   const char* result = ::getcwd(buf, PATH_MAX);
   return FilePath{String{result, String::npos, allocator}, allocator};
-}
+#elif OS(WIN)
+  char buf[MAX_PATH] = {0};
+  DWORD result = ::GetCurrentDirectoryA(MAX_PATH, buf);
+  return FilePath{String{buf, static_cast<I32>(result), allocator}, allocator};
 #endif
+}
 
 }  // namespace nu
