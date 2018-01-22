e916f46f8c4f94d9da990404ffba1534a63453d3
Tiaan Louw <tiaanl@gmail.com>
2018-01-22 02:07:49 +0100

    Add Array functionality

--- include/nucleus/Containers/Array.h
+++ include/nucleus/Containers/Array.h
@@ -9,7 +9,29 @@ namespace nu {
 template <typename T, USize Size>
 class Array {
 public:
+  using SizeType = U32;
+
   Array() {}
+
+  const T& operator[](SizeType index) const {
+    return m_data[index];
+  }
+
+  T& operator[](SizeType index) {
+    return m_data[index];
+  }
+
+  SizeType getSize() const {
+    return Size;
+  }
+
+  const T* getData() const {
+    return m_data;
+  }
+
+  T* getData() {
+    return m_data;
+  }
 
 private:
   T m_data[Size];
