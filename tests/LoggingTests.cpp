fed4656f7788b09ccc636382265dd6196dbadeda
Tiaan Louw <tiaanl@gmail.com>
2018-01-30 15:36:30 +0100

    Make sure all outputs are compiled with C++14

--- tests/LoggingTests.cpp
+++ tests/LoggingTests.cpp
@@ -10,9 +10,11 @@ TEST_CASE() {
   LOG(Info) << "testing";
   DLOG(Warning) << "this only logs in debug mode";
 
+  /*
   int a = 10;
   DCHECK(a == 11) << "a should be 10";
   DCHECK(a == 10) << "this should not show";
+  */
 }
 
 }  // namespace nu
